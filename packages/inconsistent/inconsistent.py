8a892d349f9bcecebe44271c5b85de87a27b8406
Daniele Forsi <daniele@forsi.it>
2015-12-31 20:23:21 +0100

    Import 'Package' and 'Description-md5' in a database

---
+++ packages/inconsistent/inconsistent.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+# Daniele Forsi 31/12/2015 CC0
+
+# Analyses the descriptions of Debian packages and their translations.
+#
+# For the typical usage you need at least the English descriptions and
+# one file of translations:
+# ./inconsistent.py --update en # imports Translation-en into inconsistent-en.sqlite3
+# ./inconsistent.py --update it # imports Translation-it into inconsistent-it.sqlite3
+
+import sys
+import sqlite3
+
+database_fmt = 'inconsistent-{0}.sqlite3'
+datafile_fmt = 'Translation-{0}'
+
+def usage():
+    print('Usage: {0} COMMAND ARGUMENT(S)'.format(sys.argv[0]))
+    print('  --update LANGUAGE      updates the database with the given LANGUAGE')
+
+def opt_update(language):
+    database = database_fmt.format(language)
+    datafile = datafile_fmt.format(language)
+
+    conn = sqlite3.connect(database)
+    cursor = conn.cursor()
+
+    cursor.execute("CREATE TABLE IF NOT EXISTS packages_{0} (name STRING, paragraphs INTEGER, descmd5 STRING)".format(language))
+
+    with open(datafile) as f:
+        for package in get_package(f):
+            cursor.execute("INSERT INTO packages_{0} (name, descmd5) VALUES (?, ?)".format(language), (package['Package'], package['Description-md5']))
+
+    cursor.execute("CREATE INDEX IF NOT EXISTS idx_packages_{0} ON packages_{0} (descmd5)".format(language))
+    cursor.close()
+
+    conn.commit()
+    conn.close()
+
+def get_package(f):
+    package = {}
+    for line in f.readlines():
+        if line.startswith(" "):
+            # concatenate all continuation lines
+            package[key] += "\n" + line[1:].strip("\n")
+        elif line == "\n":
+            yield package
+            package = {}
+        else:
+            key, value = line.strip("\n").split(": ", 1)
+            package[key] = value
+    # debian/control files do not end with an empty line
+    if line != "\n":
+        yield package
+
+def main():
+    if len(sys.argv) == 3 and sys.argv[1] == '--update':
+        language = sys.argv[2]
+        opt_update(language)
+    else:
+        usage()
+        sys.exit(1)
+
+if __name__ == '__main__':
+    main()
