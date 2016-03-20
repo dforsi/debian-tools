97f2925d6153c090db250084981d21a95658a80e
Daniele Forsi <daniele@forsi.it>
2016-03-20 20:45:58 +0100

    Don't invent a tag for the long description

--- packages/elasticsearch/control2json.py
+++ packages/elasticsearch/control2json.py
@@ -4,8 +4,6 @@
 
 # Import the full content of a Translation-XX file
 # in a local instance of Elasticsearch.
-# This script expects an empty line at the end of the input
-# so it doesn't work with a single debian/control file.
 
 # Usage:
 # ./control2json.py Translation-it | curl -s -XPOST localhost:9200//debian/packages/_bulk --data-binary @/dev/stdin >/dev/null
@@ -17,16 +15,22 @@ import json
 
 filename = sys.argv[1]
 
-index = json.dumps({"index": {}})
-with open(filename) as f:
-    item = {"Long-Description": ""}
+def get_package(f):
+    package = {}
     for line in f.readlines():
-        if line.startswith(" "):
-            item["Long-Description"] += line[1:]
-        elif line == "\n":
-            print(index)
-            print(json.dumps(item))
-            item = {"Long-Description": ""}
+        if line == "\n":
+            yield package
+            package = {}
+        elif line.startswith(" "):
+            # join continuation lines inserting a newline
+            # removing the leading space and the trailing newline
+            package[key] += "\n" + line[1:].strip("\n")
         else:
             key, value = line.strip("\n").split(": ", 1)
-            item[key] = value
+            package[key] = value
+
+with open(filename) as f:
+    for package in get_package(f):
+        print(json.dumps({"index": {"_id": package["Description-md5"]}}))
+        del package["Description-md5"]
+        print(json.dumps(package))
