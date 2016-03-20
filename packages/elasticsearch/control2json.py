420b9ada9c8bebcd93c88a03419325ebd35d7b61
Daniele Forsi <daniele@forsi.it>
2016-03-20 21:22:46 +0100

    Implement the update operation

--- packages/elasticsearch/control2json.py
+++ packages/elasticsearch/control2json.py
@@ -6,14 +6,15 @@
 # in a local instance of Elasticsearch.
 
 # Usage:
-# ./control2json.py Translation-it | curl -s -XPOST localhost:9200//debian/packages/_bulk --data-binary @/dev/stdin >/dev/null
+# ./control2json.py {index|update} Translation-it | curl -s -XPOST localhost:9200//debian/packages/_bulk --data-binary @/dev/stdin >/dev/null
 # Quick check:
 # curl -XGET 'localhost:9200/debian/packages/_count?pretty'
 
 import sys
 import json
 
-filename = sys.argv[1]
+action = sys.argv[1]
+filename = sys.argv[2]
 
 def get_package(f):
     package = {}
@@ -29,8 +30,20 @@ def get_package(f):
             key, value = line.strip("\n").split(": ", 1)
             package[key] = value
 
-with open(filename) as f:
+def index(filename):
     for package in get_package(f):
         print(json.dumps({"index": {"_id": package["Description-md5"]}}))
         del package["Description-md5"]
         print(json.dumps(package))
+
+def update(filename):
+    for package in get_package(f):
+        print(json.dumps({"update": {"_id": package["Description-md5"]}}))
+        del package["Description-md5"]
+        print(json.dumps({"doc": package}))
+
+with open(filename) as f:
+    if action == "index":
+        index(f)
+    elif action == "update":
+        update(f)
