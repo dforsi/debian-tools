25d2dbc06279d5bc422b22d5d0909f69ddc6f6b0
Daniele Forsi <dforsi@gmail.com>
2019-04-28 23:54:07 +0200

    Fix translation of short description of cloud-amd64

--- packages/ddtss/linux-packages.user.js
+++ packages/ddtss/linux-packages.user.js
@@ -29,7 +29,7 @@ var translations = {
       "arm64":       {"class": "macchine ARMv8 a 64 bit"},
       "armmp-lpae":  {"class": "SoC compatibili multipiattaforma ARMv7 con gestione LPAE", "longclass": "kernel ARMv7 multipiattaforma con gestione LPAE. Vedere https://wiki.debian.org/DebianKernel/ARMMP per i dettagli sulle piattaforme gestite"},
       "armmp":       {"class": "SOC compatibili multipiattaforma ARMv7", "longclass": "kernel multipiattaforma ARMv7. Vedere https://wiki.debian.org/DebianKernel/ARMMP per i dettagli sulle piattaforme gestite"},
-      "cloud-amd64": {"class": "cloud x86-64 (firmato)", "longclass": "piattaforme cloud Amazon EC2, Google Compute Engine e Microsoft Azure"},
+      "cloud-amd64": {"class": "cloud x86-64", "longclass": "piattaforme cloud Amazon EC2, Google Compute Engine e Microsoft Azure"},
       "loongson-3":  {"class": "Loongson 3A/3B", "longclass": "sistemi basati su Loongson 3A o 3B (es. da Loongson o Lemote)"},
       "marvell":     {"class": "Marvell Kirkwood/Orion", "longclass": "sistemi basati su Marvell Kirkwood e Orion (https://wiki.debian.org/ArmEabiPort#Supported_hardware)"},
       "octeon":      {"class": "Octeon", "longclass": "Cavium Networks Octeon"},
